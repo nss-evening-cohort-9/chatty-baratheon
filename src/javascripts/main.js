17ef5a451027ce852bf32b202887e066c2abb302
Heath JH Moore <heathjhmoore@gmail.com>
2019-04-30 21:31:09 -0500

    imported event listener to main

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,10 +2,12 @@ import '../styles/main.scss';
 import 'bootstrap';
 
 import chatBox from './components/chatBox';
+import eventListeners from './components/eventListeners';
 
 
 const init = () => {
   chatBox.initializeMessages();
+  eventListeners.eventListeners();
 };
 
 init();
