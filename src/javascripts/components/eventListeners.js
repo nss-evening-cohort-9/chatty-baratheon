b5c286cf8bae0d07c083706f5e336efa16554a13
Lakia Jones <rachandra_jones@yahoo.com>
2019-05-02 19:01:10 -0500

    WIP

--- src/javascripts/components/eventListeners.js
+++ src/javascripts/components/eventListeners.js
@@ -9,4 +9,18 @@ const eventListeners = () => {
   $('#chatInput').keyup(chatBox.newMessageEvent);
 };
 
+const sizeIncrease = () => {
+  $('#increaseSize').click(sizeIncrease() {
+    curSize = parseInt($('#content').css('font-size')) + 2;
+    if (curSize <= 32)
+      $('#content').css('font-size', curSize);
+  });
+};
+
+const init = () => {
+  sizeIncrease();
+};
+
+init();
+
 export default { eventListeners };
